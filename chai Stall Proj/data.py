29ee3f1e10f250a66c8f5fd70231f57564618654
Devesh <kadamdevesh969@gmail.com>
2026-07-31 13:51:43 +0530

    Chai commit

---
+++ chai Stall Proj/data.py
@@ -0,0 +1,122 @@
+menu_items = [
+  {
+    "id": 1,
+    "name": "Masala Chai",
+    "category": "chai",
+    "price": 20.0,
+    "description": "Classic Indian tea brewed with aromatic spices",
+    "available": True
+  },
+  {
+    "id": 2,
+    "name": "Ginger Chai",
+    "category": "chai",
+    "price": 25.0,
+    "description": "Fresh ginger infused tea",
+    "available": True
+  },
+  {
+    "id": 3,
+    "name": "Elaichi Chai",
+    "category": "chai",
+    "price": 25.0,
+    "description": "Cardamom flavored milk tea",
+    "available": True
+  },
+  {
+    "id": 4,
+    "name": "Cutting Chai",
+    "category": "chai",
+    "price": 15.0,
+    "description": "Mumbai-style half glass strong tea",
+    "available": True
+  },
+  {
+    "id": 5,
+    "name": "Lemon Chai",
+    "category": "chai",
+    "price": 20.0,
+    "description": "Refreshing black tea with lemon",
+    "available": True
+  },
+  {
+    "id": 6,
+    "name": "Green Chai",
+    "category": "chai",
+    "price": 30.0,
+    "description": "Healthy antioxidant-rich green tea",
+    "available": True
+  },
+  {
+    "id": 7,
+    "name": "Cold Coffee",
+    "category": "coffee",
+    "price": 80.0,
+    "description": "Chilled coffee blended with milk and ice",
+    "available": True
+  },
+  {
+    "id": 8,
+    "name": "Filter Coffee",
+    "category": "coffee",
+    "price": 40.0,
+    "description": "South Indian filter coffee",
+    "available": True
+  },
+  {
+    "id": 9,
+    "name": "Bun Maska",
+    "category": "snacks",
+    "price": 35.0,
+    "description": "Soft bun loaded with butter",
+    "available": True
+  },
+  {
+    "id": 10,
+    "name": "Vada Pav",
+    "category": "snacks",
+    "price": 30.0,
+    "description": "Mumbai's famous potato fritter burger",
+    "available": True
+  },
+  {
+    "id": 11,
+    "name": "Samosa",
+    "category": "snacks",
+    "price": 25.0,
+    "description": "Crispy pastry stuffed with spicy potatoes",
+    "available": True
+  },
+  {
+    "id": 12,
+    "name": "Kanda Bhaji",
+    "category": "snacks",
+    "price": 40.0,
+    "description": "Crispy onion fritters served hot",
+    "available": False
+  },
+  {
+    "id": 13,
+    "name": "Veg Puff",
+    "category": "snacks",
+    "price": 35.0,
+    "description": "Flaky pastry with spicy vegetable filling",
+    "available": True
+  },
+  {
+    "id": 14,
+    "name": "Chocolate Biscuit",
+    "category": "biscuit",
+    "price": 10.0,
+    "description": "Crunchy chocolate cream biscuits",
+    "available": True
+  },
+  {
+    "id": 15,
+    "name": "Rusk",
+    "category": "biscuit",
+    "price": 15.0,
+    "description": "Crispy toasted bread perfect with chai",
+    "available": True
+  }
+]
--- chai Stall Proj/main.py
+++ chai Stall Proj/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query
+from data import menu_items
+from model import MenuItems, MenuResponse
 
 app = FastAPI(
     title='Chai Point API',
@@ -9,3 +11,20 @@ app = FastAPI(
 def root():
     return {'message' : 'Welcome to the Root page for Chai Point'}
 
+@app.get('/menu', response_model=MenuResponse)
+def get_menu_items(category : str | None = Query(None, description='Filter by chai, coffee or combo')):
+    if category:
+        filtered = [item for item in menu_items if item['category'].lower() == category.lower()]
+        if filtered:
+            return MenuResponse(count = len(filtered), items = filtered)
+        raise HTTPException(status_code=404, detail=f'No cateogey {category} found in menu items')
+    return MenuResponse(count=len(menu_items), items=menu_items)
+        
+
+@app.get('/menu/{id}', response_model=MenuItems)
+def get_menu_item_by_id(id : int):
+    for item in menu_items:
+        if item["id"] == id:
+            return item
+    raise HTTPException(status_code=404, detail=f'The menu item with id {id} is not found')
+        
